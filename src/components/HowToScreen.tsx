3bfb72090904b092b606f3a1527b8cf5898304ac
Natanel Birarov <63101763+NatanelBirarov@users.noreply.github.com>
2025-10-22 19:47:05 +0300

    feat: implement CSS modules for modal components and refactor GameScreen and SettingsScreen

--- src/components/HowToScreen.tsx
+++ src/components/HowToScreen.tsx
@@ -1,13 +1,15 @@
 import Button from "./Button/Button";
 import Modal from "./Modal/Modal";
 
+import { modalText } from "./Modal/Modal.module.css";
+
 type HowToScreenProps = {
   onClose: () => void;
 };
 
 export default function HowToScreen({ onClose }: HowToScreenProps) {
   return (
-    <Modal className="how-to-screen">
+    <Modal type="howToModalContent">
       <p>
         <strong>1. </strong> Select the next available set.
       </p>
@@ -22,7 +24,7 @@ export default function HowToScreen({ onClose }: HowToScreenProps) {
         <strong>4. </strong> Clear all levels to unlock the next set!
       </p>
       <Button type="modal" onClick={onClose}>
-        <div className="modal-button-text">Close</div>
+        <div className={modalText}>Close</div>
       </Button>
     </Modal>
   );
